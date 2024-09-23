3bcd5ad010fb45b8f5c9e078cf373b69cf08c656
w111zard <w111zard111@gmail.com>
2024-09-23 15:29:37 +0300

    refactoring

--- src/snitcher.js
+++ src/snitcher.js
@@ -1,11 +1,6 @@
 const { getLinks, handleLinks } = require('./utils/snithcer');
 
 function snitcher(options, callback) {
-  if (!options.file) {
-    // Remember Zalgo?
-    return process.nextTick(() => callback(new Error('File was not provided')));
-  }
-
   getLinks(options.file, (err, links) => {
     if (err) return callback(err);
 
