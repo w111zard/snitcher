25e67f3c9a5e124b69b2bd5abd56e2892d42d3bd
w111zard <w111zard111@gmail.com>
2024-09-25 12:14:41 +0300

    wrapped code from index.js into start function

--- src/index.js
+++ src/index.js
@@ -3,45 +3,49 @@
 const Snitcher = require('./snitcher');
 const { getArgs, getLinks } = require('./utils/common');
 
-const { file, mode, concurrency, help } = getArgs();
+function start() {
+  const { file, mode, concurrency, help } = getArgs();
 
-if (help) {
-  console.log('Usage: snitcher [options] <file>');
-  console.log(`  -m, --mode\tAsync execution mode 
+  if (help) {
+    console.log('Usage: snitcher [options] <file>');
+    console.log(`  -m, --mode\tAsync execution mode 
     (s - sequential, p - parallel, lp - limited parallel, tq - task queue)\n`);
-  console.log('  -c, --concurrency Maximum number of parallel async operations');
-  return process.exit(0);
-}
-
-if (!file) {
-  console.log('Please, provide file containing links!');
-  return process.exit(1);
-}
-
-if (mode && !Snitcher.getModes()[mode]) {
-  console.log('Invalid mode');
-  return process.exit(1);
-}
-
-if (concurrency && concurrency < 1) {
-  console.log('Concurrency must be greater than 0');
-  return process.exit(1);
-}
-
-getLinks(file, (err, links) => {
-  if (err) {
-    console.log(err.message);
-    process.exit(1);
+    console.log('  -c, --concurrency Maximum number of parallel async operations');
+    return process.exit(0);
   }
 
-  const snitcher = new Snitcher({ mode, concurrency });
-  snitcher.process(links, (err) => {
+  if (!file) {
+    console.log('Please, provide file containing links!');
+    return process.exit(1);
+  }
+
+  if (mode && !Snitcher.getModes()[mode]) {
+    console.log('Invalid mode');
+    return process.exit(1);
+  }
+
+  if (concurrency && concurrency < 1) {
+    console.log('Concurrency must be greater than 0');
+    return process.exit(1);
+  }
+
+  getLinks(file, (err, links) => {
     if (err) {
-      console.log('Error:');
-      console.log(err);
+      console.log(err.message);
+      process.exit(1);
     }
-    else {
-      console.log('Done!');
-    }
+
+    const snitcher = new Snitcher({ mode, concurrency });
+    snitcher.process(links, (err) => {
+      if (err) {
+        console.log('Error:');
+        console.log(err);
+      }
+      else {
+        console.log('Done!');
+      }
+    });
   });
-});
+}
+
+start();
